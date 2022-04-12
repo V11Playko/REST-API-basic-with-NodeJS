d95bf0fe8c8c3577cb143bdc66120400b4d38a7b
V11Playko <heinnervega20@gmail.com>
2022-04-12 07:35:06 -0500

    Haciendo las rutas

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const app = express();
 const morgan = require('morgan');
 
 // Settings 
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 // Middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Routes
+app.use(require('./routes/index'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
