d54ad7cf3a943d595e756886fa4cd3018d367684
V11Playko <heinnervega20@gmail.com>
2022-04-13 08:09:43 -0500

    Probando la REST-API

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(express.json());
 // Routes
 app.use(require('./routes/index'));
 app.use('/api/movies',require('./routes/movies'));
+app.use('/api/users',require('./routes/users'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
